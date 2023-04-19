4bb73b98b61bfeb7cfa695a881e00ce21d128956
castor12m <32126292+castor12m@users.noreply.github.com>
2023-04-19 23:58:41 +0900

    노가다 상품입력 신기한 한글 변수명 사용

--- fresh/app/list/page.js
+++ fresh/app/list/page.js
@@ -3,17 +3,28 @@ export default function List() {
     let name = 'park'
     let age = 20
     let link = "http://www.google.co.kr"
-  
+
+    let 상품 = ['Tomatoes', 'Pasta', 'Coconut']
+    let 변수 = 20
+
+    console.log('test-------------')
+    console.log('test-------------' + {상품})
+    console.log({상품})
+
     return (
       <div>
-        <h4 className="title">뤼스트</h4>
+        <h4 className="title">List</h4>
        
-       <div className="food">
-        <h4>상품1 $40</h4>
-       </div>
-       <div className="food">
-        <h4>상품2 $40</h4>
-       </div>
+        <div className="food">
+          <h4>{상품[0]} $40</h4>
+        </div>
+        <div className="food">
+          <h4>{상품[1]} $40</h4>
+        </div>
+        <div className="food">
+          <h4>{상품[2]} $40</h4>
+        </div>
+
       </div>
     )
   }
