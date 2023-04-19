7aed3eb54028cc4e5eade8bc8835ab7666f5debb
castor12m <32126292+castor12m@users.noreply.github.com>
2023-04-20 00:22:33 +0900

    여러가지 이미지 삽입

--- fresh/app/list/page.js
+++ fresh/app/list/page.js
@@ -1,6 +1,3 @@
-import Image from "next/image"
-import 작명 from '/public/food/test0.png'
-
 export default function List() {
 
     let name = 'park'
@@ -8,17 +5,6 @@ export default function List() {
     let link = "http://www.google.co.kr"
 
     let 상품 = ['Tomatoes', 'Pasta', 'Coconut']
-    let 변수 = 20
-
-    console.log('test-------------')
-    console.log('test-------------' + {상품})
-    console.log({상품})
-
-    let b = 상품.map((a)=>{
-      
-    })
-
-    console.log(b)
 
     return (
       <div>
@@ -27,7 +13,7 @@ export default function List() {
           상품.map((a, i)=>{           
             return (
               <div className="food" key={i}>
-                <Image src={작명} className="food-img"/>
+                <img src={`/food/test${i}.png`} className="food-img"/>
                 <h4>{a} $40</h4>
               </div>
             )    
